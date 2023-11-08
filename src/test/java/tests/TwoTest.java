32b0a75d4caadcff699d2f881ac530842fd13a7c
amullk <141050920+amullk@users.noreply.github.com>
2023-11-09 00:05:46 +0530

    Updated by krish

--- src/test/java/tests/TwoTest.java
+++ src/test/java/tests/TwoTest.java
@@ -10,6 +10,7 @@ import resources.Base;
 public class TwoTest extends Base {
 	@Test
 	public void twoTest() throws IOException, InterruptedException {
+		System.out.println("Krish has updated the code");
 		System.out.println("TwoTest");
 		WebDriver driver = initializeDriver();
 		driver.get("https://tutorialsninja.com/demo/");
